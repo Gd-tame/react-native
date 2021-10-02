5623fc6e5e66102c25866824e4afab0e06c337d4
Granth Dhir <granth.dhir@gmail.com>
2021-10-03 01:57:01 +0530

    pushing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "header">
+      <div className = "bar">
+      <ul type = "None" className = "nav">
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact</li>
+      </ul>
+      </div>
+      <div className = "main">
+        <div className = "content">
+          <img src = "https://upload.wikimedia.org/wikipedia/commons/f/fb/Schematicky_atom.png"></img>
+        </div>
+        <div className = "form">
+          <form>
+            <div className = "Login">
+            <label for = "Login">Login Id</label>
+            <input type = "text" id = "Login"></input>
+            </div>
+            <div>
+            <label for = "Password">Password</label>
+            <input type = "password" id = "Password"></input>
+            </div>
+            <div className = "button">
+            <button type ="Submit">Submit</button>
+            </div>
+
+          </form>
+        </div>
+      </div>
+      <footer>
+        <p>Just a fun project</p>
+        <img src = "https://iiche.netlify.app/img/logoWhite.webp"></img>
+      </footer>
+
     </div>
+
   );
 }
 
